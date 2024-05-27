19bfa25b549492325d3132df1c27396e108f9e51
e-prashanth <edeprashanth@gmail.com>
2024-05-27 11:05:10 +0530

    completed the MainPage functionality

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainPage.css";
+import { useNavigate } from "react-router-dom";
 function MainPage() {
+  const [player1Name, setplayer1Name] = useState("Player1");
+  const [player2Name, setplayer2Name] = useState("Player2");
+  const navigate = useNavigate();
+  const handleStartGame = (event) => {
+    event.preventDefault();
+    console.log("starting the game !!");
+    console.log(player1Name);
+    console.log(player2Name);
+    navigate("/game");
+  };
   return (
     <div className="MainContainer">
-      <form className="FormContianer">
+      <form
+        className="FormContianer"
+        onSubmit={(e) => {
+          handleStartGame(e);
+        }}
+      >
         <div className="onlyInputsContainer">
           <div className="InputContainer">
             <label>Player 1</label>
-            <input type="text" placeholder="Enter the Player1 Name" />
+            <input
+              type="text"
+              required
+              placeholder="Enter the Player1 Name"
+              onChange={(e) => {
+                setplayer1Name(e.target.value);
+              }}
+            />
           </div>
           <div className="InputContainer">
             <label>Player 2</label>
-            <input type="text" placeholder="Enter the Player2 Name" />
+            <input
+              type="text"
+              required
+              placeholder="Enter the Player2 Name"
+              onChange={() => {
+                setplayer2Name(event.target.value);
+              }}
+            />
           </div>
         </div>
 
         <div className="buttonContainer">
-          <button>Start the Game</button>
+          <button type="submit">Start the Game</button>
         </div>
       </form>
     </div>
