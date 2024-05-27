e3de6b5aaf089f02e149b3d7babd71b8cf217bd9
e-prashanth <edeprashanth@gmail.com>
2024-05-27 17:57:50 +0530

    checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+// import React from 'react'
+// import { Route, Router, Routes } from 'react-router-dom'
+// import MainPage from './Pages/MainPage.jsx';
+// import GamePage from './Pages/GamePage.jsx';
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path='/TikTakToe/' element={<MainPage/>}/>
+//       <Route path='/TikTakToe/game' element={<GamePage/>}/>
+//     </Routes>
+//   )
+// }
+
+// export default App;
 import React from 'react'
-import { Route, Router, Routes } from 'react-router-dom'
-import MainPage from './Pages/MainPage.jsx';
-import GamePage from './Pages/GamePage.jsx';
+
 function App() {
   return (
-    <Routes>
-      <Route path='/TikTakToe/' element={<MainPage/>}/>
-      <Route path='/TikTakToe/game' element={<GamePage/>}/>
-    </Routes>
+    <div>App</div>
   )
 }
 
